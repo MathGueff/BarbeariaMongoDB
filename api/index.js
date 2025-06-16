f2457848d607bc6d5f923083556a3d642d58d5a4
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-06-16 17:16:03 -0300

    Correção no css do swagger

--- api/index.js
+++ api/index.js
@@ -9,7 +9,6 @@ import swaggerUI from 'swagger-ui-express'
 
 const app = express()
 const PORT = process.env.PORT || 3000
-const CSS_URL = "/swagger-ui.css" // Caminho relativo à pasta 'public'
 
 app.use(cors()) //Habilita o CORS Cross-Origin resource sharing
 app.use(express.json())//parse do JSON
@@ -24,11 +23,7 @@ const swaggerFilePath = path.resolve('api/swagger/swagger_output.json')
 const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf8'))
 
 // Rota da documentação 
-app.use('/api/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument, {
-    customCss:
-        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-    customCssUrl: CSS_URL
-}))
+app.use('/api/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 //define o favicon
 app.use('/favicon.ico', express.static('public/images/favicon.png'))
